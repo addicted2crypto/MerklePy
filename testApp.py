0da6117a1102ae2117c8cecec6b53ccef15dbe6b
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-09-02 16:44:02 -0700

    testing app py

--- testApp.py
+++ testApp.py
@@ -1,14 +1,15 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from python_dotenv import load_dotenv
+# load_dotenv()
 
 from web3 import Web3
-infura_url = os.getenv("INFURA_URL")
+infura_url ="NEVER_SHARE_YOUR_INFURA_URL"
+# os.getenv("INFURA_URL")
 
 web3 = Web3(Web3.HTTPProvider(infura_url))
 print(web3.is_connected())
 
 print(web3.eth.block_number)
 
-balance = web3.eth.get_balance("0x742d35Cc6634C0532925a3b844Bc454e4438f44e")
+balance = web3.eth.get_balance("0x616767179c5305a89f13348134C681061Cf0bA9e")
 print(web3.from_wei(balance, 'ether'))
