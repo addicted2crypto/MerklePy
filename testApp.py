d6e3512130e57c05dd050d470b9f3093bd8b5340
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-09-02 22:40:06 -0700

    Avax structure initiated also

--- testApp.py
+++ testApp.py
@@ -3,13 +3,22 @@ import os
 # load_dotenv()
 
 from web3 import Web3
-infura_url ="NEVER_SHARE_YOUR_INFURA_URL"
+infura_url ="NEVER PUT YOUR INFURA URL IN A PUBLIC REPO"
 # os.getenv("INFURA_URL")
-
+AVAX_RPC_URL = "https://api.avax.network/ext/bc/C/rpc" 
+w3 = Web3(Web3.HTTPProvider(AVAX_RPC_URL))
 web3 = Web3(Web3.HTTPProvider(infura_url))
 print(web3.is_connected())
 
 print(web3.eth.block_number)
 
-balance = web3.eth.get_balance("0x616767179c5305a89f13348134C681061Cf0bA9e")
+balance = web3.eth.get_balance("0x455bF23eA7575A537b6374953FA71B5F3653272c") #FAKE PHISHING BULLSHIT ASSHOLE
 print(web3.from_wei(balance, 'ether')) #even avax is called ether thats cool
+# print(web3.eth.get_proof('latest', [0], True))
+
+#avax info
+print(w3.is_connected())    
+print(w3.eth.block_number)
+
+AVAX_balance = w3.eth.get_balance("0x134A48C4Bc6c4C658bd7416A298498da146bbF0A") #Avax 
+print(w3.from_wei(AVAX_balance, 'ether'))
