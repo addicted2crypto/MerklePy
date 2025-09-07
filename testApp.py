90e4dc3d394d07ed3c5dddbf19d651fea80d2ab4
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-09-06 20:42:12 -0700

    evm working

--- testApp.py
+++ testApp.py
@@ -1,7 +1,7 @@
+import time
 import os
 import json
 from web3 import Web3
-
 from dotenv import load_dotenv
 load_dotenv()
 infura_url = os.getenv("INFURA_URL")
@@ -10,17 +10,6 @@ AVAX_RPC_URL = "https://api.avax.network/ext/bc/C/rpc"
 
 w3 = Web3(Web3.HTTPProvider(AVAX_RPC_URL))
 web3 = Web3(Web3.HTTPProvider(infura_url))
-
-phising_addresses =["0x19Fa72D9D076668CeD11399BaE149F916938BD8D","0x455bF23eA7575A537b6374953FA71B5F3653272c" ] #will add addresses to watch here
-print(web3.is_connected())
-
-print(web3.eth.block_number)
-
-balance = web3.eth.get_balance(phising_addresses) #another PHISSHING address
-() #FAKE PHISHING BULLSHIT ASSHOLE
-print(web3.from_wei(balance, 'ether')) #even avax is called ether thats cool
-# print(web3.eth.get_proof('latest', [0], True))
-
 #avax info
 print(w3.is_connected())    
 print(w3.eth.block_number)
@@ -28,12 +17,41 @@ print(w3.eth.block_number)
 AVAX_balance = w3.eth.get_balance("0x134A48C4Bc6c4C658bd7416A298498da146bbF0A") #Avax 
 print(w3.from_wei(AVAX_balance, 'ether'))
 
+phising_addresses =["0x19Fa72D9D076668CeD11399BaE149F916938BD8D","0x455bF23eA7575A537b6374953FA71B5F3653272c" ] #will add addresses to watch here
+print(web3.is_connected())
+
+print(web3.eth.block_number)
+
+def handle_transaction(transaction):
+
+    if transaction["from"] in phising_addresses or transaction["to"] in phising_addresses:
+        print(f"Phishing address detected: {transaction['from']} -> {transaction['to']}")
+
+block_filter = web3.eth.filter('latest')
+print(block_filter)
+print(phising_addresses.balance.from_wei('ether', ether))
+while True:
+    block_number = web3.eth.block_number
+    transactions = web3.eth.get_block(block_number, full_transactions=True).transactions
+
+    for transactions in transactions:
+        handle_transaction(transactions)
+
+    time.sleep(1)    
+balance = web3.eth.get_balance(phising_addresses) #another PHISSHING address
+#FAKE PHISHING BULLSHIT ASSHOLE
+print(web3.from_wei(balance, 'ether')) #even avax is called ether thats cool
+# print(web3.eth.get_proof('latest', [0], True))
+
+
+
 
 
 # Smart Contract Reconsruction
 abi = json.loads('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"chainId","type":"uint256"}],"name":"AddSupportedChainId","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"newBridgeRoleAddress","type":"address"}],"name":"MigrateBridgeRole","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"address","name":"feeAddress","type":"address"},{"indexed":false,"internalType":"uint256","name":"feeAmount","type":"uint256"},{"indexed":false,"internalType":"bytes32","name":"originTxId","type":"bytes32"},{"indexed":false,"internalType":"uint256","name":"originOutputIndex","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"chainId","type":"uint256"}],"name":"Unwrap","type":"event"},{"inputs":[{"internalType":"uint256","name":"chainId","type":"uint256"}],"name":"addSupportedChainId","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burnFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"chainIds","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newBridgeRoleAddress","type":"address"}],"name":"migrateBridgeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"address","name":"feeAddress","type":"address"},{"internalType":"uint256","name":"feeAmount","type":"uint256"},{"internalType":"bytes32","name":"originTxId","type":"bytes32"},{"internalType":"uint256","name":"originOutputIndex","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"chainId","type":"uint256"}],"name":"unwrap","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 
-address = "0x152b9d0FdC40C096757F570A51E494bd4b943E50" #wrapped btc
+address = "0xC654721fBf1F374fd9FfA3385Bba2F4932A6af55" #juicy
+# "0x152b9d0FdC40C096757F570A51E494bd4b943E50" #wrapped btc
 
 contract = w3.eth.contract(address=address, abi=abi) 
 
