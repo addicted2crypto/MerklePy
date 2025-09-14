7529b1647a337ba1bfb79b00195ae0de33ca3aa4
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-09-13 17:56:55 -0700

    DIscord implementation with avax script

--- discordBots/avaxTrackerDiscord.py
+++ discordBots/avaxTrackerDiscord.py
@@ -1,1 +1,12 @@
 import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+    channel = client.get_channel.send("Here is the lates AVAX wallet cluster and top trader analysis...")
+
+    #run with token
+    client.run('YOUR_DISCORD_TOKEN')
